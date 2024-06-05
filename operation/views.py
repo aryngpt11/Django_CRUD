730222f7a1b793fb32b601ced4c3024c17293cde
VishalKumar1111 <aryan11gs@gmail.com>
2024-06-05 13:13:35 +0530

    CRUD operations

--- operation/views.py
+++ operation/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+""" from django.shortcuts import render
 import io
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
-from operation.models import *
+from operation.models import Student
 from operation.serializers import StudentSerializer
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 def student_api(request):
     if request.method == 'GET':
@@ -22,4 +22,76 @@ def student_api(request):
         stu=Student.object.all()
         serializer=StudentSerializer(stu,many=True)
         json_data=JSONRenderer().render(serializer.data)
-        return HttpResponse(json_data,content_type='application/json')
+        return HttpResponse(json_data,content_type='application/json') """
+
+from django.shortcuts import render
+import io
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from operation.models import Student
+from operation.serializers import StudentSerializer
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
+def student_api(request):
+    if request.method == 'GET':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id', None)
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            json_data = JSONRenderer().render(serializer.data)
+            return HttpResponse(json_data, content_type='application/json')
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data, content_type='application/json')
+
+#check the method type like get or post
+    if request.method == 'POST':
+        # data comes in form of JSON; first convert it into the Python complex type and after saving it we also have to convert it into JSON type to send to the client
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data=pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data Inserted'}
+            # converted into JSON
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type='application/json')
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json')
+    if request.method == 'PUT':
+        json_data=request.body
+        stream=io.BytesIO(json_data)
+        pythondata=JSONParser().parse(stream)
+        id=pythondata.get('id')
+        stu=Student.objects.get(id=id)
+        #partial update
+        serializer=StudentSerializer(stu,data=pythondata,partial=True)#stores complex data or model objexts
+        #serializer=StudentSerializer(stu,data=pythondata) #fully Updated
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data Updated'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type='application/json')
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json')
+
+
+    if request.method == 'DELETE':
+        json_data=request.body
+        stream=io.BytesIO(json_data)
+        pythondata=JSONParser().parse(stream)
+        id=pythondata.get('id')
+        stu=Student.objects.get(id=id) #object get
+        stu.delete()
+        res = {'msg': 'Data Deleted'}
+        json_data = JSONRenderer().render(res)
+        return HttpResponse(json_data, content_type='application/json')
+
+
+
